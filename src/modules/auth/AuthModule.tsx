b8c3c220f50c239860ccf097381e23d721c69aaf
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2024-01-30 21:59:05 +0200

    update

--- src/modules/auth/AuthModule.tsx
+++ src/modules/auth/AuthModule.tsx
@@ -1,9 +1,11 @@
 import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
+import AuthSignup from "../auth-signup/AuthSignup";
 
 const AuthLogin = lazy(() => import("../auth-login/AuthLogin"));
 
 export const AuthModule = () => {
+
   return (
     <Routes>
       <Route
@@ -18,7 +20,7 @@ export const AuthModule = () => {
         path={`/signup`}
         element={
           <Suspense fallback={"Loading"}>
-            <AuthLogin />
+            <AuthSignup />
           </Suspense>
         }
       />
